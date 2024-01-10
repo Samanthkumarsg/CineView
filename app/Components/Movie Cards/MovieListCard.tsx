6a63f99b56054ba1e2662eaa1574b0cf4bce5db4
samanthkumarsg <samanthkumarsg@gmail.com>
2024-01-10 19:01:02 +0100

    Feature: Added Movie card component

--- app/Components/Movie Cards/MovieListCard.tsx
+++ app/Components/Movie Cards/MovieListCard.tsx
@@ -1,15 +1,13 @@
 export default function MovieListCard(){
     return(
-        
             <div className='flex flex-col items-start justify-center mt-2 border-b py-8 w-full'>
                     <h1 className='leading-relaxed text-2xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-slate-700 to-red-700 mb-2'>
-                    Best of Sweden: WIE IM HIMMEL [As it is in heaven / Så som i himmelen] [OmeU]
+                        Best of Sweden: WIE IM HIMMEL [As it is in heaven / Så som i himmelen] [OmeU]
                      </h1>
                     <div className='flex flex-row items-center justify-between w-full'>
                       <div className='text-xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-slate-700 to-red-700 mt-4'>@ Kino 1 </div>
                       <h1 className='text-lg font-semibold text-gray-600 mt-4'>Mi, 10.01.2024 17:00 </h1>
                     </div>
                 </div>
-
     )
 }
